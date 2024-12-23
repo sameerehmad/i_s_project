f4f1e4961b9bbd9704597024b02badbc96dc917b
UnknownOne9 <sameer.nctp@gmail.com>
2024-12-24 00:04:03 +0500

    Seed added to add dummy records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+[{"name"=>"Ahmad",
+  "email"=>"ahmad@example.com",
+  "cnic"=>"12345-1234567-2"},
+ {"name"=>"Daniyaal",
+  "email"=>"daniyaal@example.com",
+  "cnic"=>"12345-1234567-9"},
+ {
+  "name"=>"Imraan",
+  "email"=>"imraan@example.com",
+  "cnic"=>"12345-1234567-0"},
+ {"name"=>"Sameer",
+  "email"=>"sameer@example.com",
+  "cnic"=>"12345-1234567-3"}].each do |user_data|
+        User.find_or_create_by(name: user_data["name"], email: user_data["email"], cnic: user_data["cnic"])
+  end
